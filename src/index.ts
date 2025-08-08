5847d1108ae00320a2b258852d9f99c0667ae546
Shashwat Tripathi <shashwattripathi0173@gmail.com>
2025-08-08 15:02:10 +0530

    feat: implement basic WebSocket server with room-based chat support
    - Users can join rooms via "join" message
    - Messages sent with type "chat" are broadcast to users in the same room

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,40 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let allSockets: WebSocket[] = [];
+interface User {
+  socket: WebSocket;
+  room: string;
+}
+
+let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-  allSockets.push(socket);
-
   socket.on("message", (message) => {
-    console.log("message received " + message.toString());
-    allSockets.forEach((socket) => {
-      socket.send(message.toString());
-    });
-  });
+    // @ts-ignore
+    const parsedMessage = JSON.parse(message);
+    if (parsedMessage.type == "join") {
+      console.log("user joined room " + parsedMessage.payload.roomId);
+      allSockets.push({
+        socket,
+        room: parsedMessage.payload.roomId,
+      });
+    }
 
-  socket.on("disconnect", () => {
-    allSockets = allSockets.filter(x => x != socket);
-  })
+    if (parsedMessage.type == "chat") {
+      console.log("user wants to chat");
+      // const currentUserRoom = allSockets.find((x) => x.socket == socket).room
+      let currentUserRoom = null;
+      for (let i = 0; i < allSockets.length; i++) {
+        if (allSockets[i].socket == socket) {
+          currentUserRoom = allSockets[i].room;
+        }
+      }
+
+      for (let i = 0; i < allSockets.length; i++) {
+        if (allSockets[i].room == currentUserRoom) {
+          allSockets[i].socket.send(parsedMessage.payload.message);
+        }
+      }
+    }
+  });
 });
